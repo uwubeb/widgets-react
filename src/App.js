b3e0f24c94d53da2ea5d1f7f6698510895406fdd
Arūnas Zamalis <8914148+uwubeb@users.noreply.github.com>
2022-06-01 18:13:19 +0300

    Dropdown widget done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Accordion from './components/Accordion';
 import Search from './components/Search';
-
+import Dropdown from './components/Dropdown';
 const items = [
   {
     title: 'What is React?',
@@ -16,11 +16,40 @@ const items = [
     content: 'You use React by creating components',
   },
 ];
+
+const options = [
+  {
+    label: 'The color Red',
+    value: 'red',
+  },
+  {
+    label: 'The color Green',
+    value: 'green',
+  },
+  {
+    label: 'And the color Blue',
+    value: 'blue',
+  },
+];
+// eslint-disable-next-line import/no-anonymous-default-export
 export default () => {
+  const [selected, setSelected] = useState(options[0]);
+  const [showDropdown, setShowDropdown] = useState(true);
+
   return (
     <div>
       {/* <Accordion items={items} /> */}
-      <Search />
+      {/* <Search /> */}
+      <button onClick={() => setShowDropdown(!showDropdown)}>
+        Toggle Dropdown
+      </button>
+      {showDropdown ? (
+        <Dropdown
+          selected={selected}
+          onSelectedChange={setSelected}
+          options={options}
+        />
+      ) : null}
     </div>
   );
 };
